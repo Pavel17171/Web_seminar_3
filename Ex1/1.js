d7759e7d3d5f9a258524f4f8f56373c675069341
[1] <[1.asus.rfrf]>
2023-02-08 11:38:33 +0300

    Add forder Ex1 with task 1 and folder Ex2 with task 2

---
+++ Ex1/1.js
@@ -0,0 +1,40 @@
+﻿// Функция запроса температуры в градусах цельсия
+function interTemp() {
+    var temperature = +prompt('Введите температуру по Цельсию: ');
+    return temperature;
+}
+
+// Функция перевода градусов цельсия в градусы по фаренгейту
+function temperatureConvert(interNum) {
+    var tempFar = (9 / 5) * interNum + 32;
+    tempFar = +tempFar.toFixed(2);
+    return tempFar;
+}
+
+// Функция вывода сообщения с исходными данными и результатом перевода
+function printRes() {
+    var a = interTemp();
+    var b = temperatureConvert(a);
+    alert(`Цельсий: ${a}, Фаренгейт: ${b}`);
+}
+
+// Функция вопроса на повторение перевода градусов
+function question() {
+    ans = confirm('Ещё? ');
+    return ans;
+}
+
+// Функция для работы, объединяющая другие функции
+function convert() {
+    alert('Добрый день!')
+    printRes();
+    answer = question();
+    while (answer) {
+        printRes();
+        answer = question();
+    }
+    alert('До новых встреч!');
+}
+
+// Вызов функции
+convert();
---
+++ Ex2/2.js
@@ -0,0 +1,7 @@
+function greeting(interName) {
+    console.log(`Welcome, ${interName}`);
+}
+
+const nameUser = prompt('What is your name? ');
+
+greeting(nameUser);
